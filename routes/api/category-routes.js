c291375674c9e3170e52a5c4979302f902bdd256
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-01 21:05:38 -0700

    add post route for new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,8 +31,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
