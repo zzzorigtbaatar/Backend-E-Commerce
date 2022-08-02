bc3263476534f38810e4bb51326732b617d333bb
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-01 21:06:03 -0700

    add put route for updating category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,8 +41,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
