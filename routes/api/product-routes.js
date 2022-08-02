c290c9c525dab67c4e9fc2e1bed3740cfd3a3cd5
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-01 21:11:41 -0700

    add get route for all tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -102,7 +102,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
@@ -113,7 +113,6 @@ router.delete('/:id', async (req, res) => {
     const productData = await Product.destroy({
       where: { id: req.params.id },
     });
-    console.log(productData);
     if (!productData) {
       res.status(404).json({ message: "No product found with that id!" });
       return;
