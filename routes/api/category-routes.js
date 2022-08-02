4d77971ea6d82b963f4d8f98b6cacbbd73d5fe1a
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-01 21:04:26 -0700

    add get route for single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,9 +15,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
