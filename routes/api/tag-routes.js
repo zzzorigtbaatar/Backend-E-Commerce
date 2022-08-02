7baa9263df3f96ad69c232434ae6088a486f7828
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-01 21:12:04 -0700

    add get route for single tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,9 +15,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
